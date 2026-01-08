193b88fd489d6eca7201f4e8a806482f6449594d
kodd714 <kenkendiansah@gmail.com>
2026-01-08 20:20:26 +0700

    Delete meme_dictionary.py

--- meme_dictionary.py
+++
@@ -1,7 +0,0 @@
-meme_dict = {"CRINGE": "Sesuatu yang sangat aneh atau memalukan","LOL": "Tanggapan umum terhadap sesuatu yang lucu", "ROFL": "tanggapan terhadap lelucon", "AGGRO": "untuk menjadi agresif/marah", "CREEPY": "menakutkan, tidak menyenangkan"}
-word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
-
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print("kata tidak ditemukan di dalam kamus")
